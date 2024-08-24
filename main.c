a026d61f96b2ed082abb73a98794537839847bc5
rouisaek <rouisaek@outlook.com>
2024-08-24 10:10:00 +0100

    8/24/24

--- main.c
+++ main.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
-// #include "mimic.h"
 
-char chartoupper(char ch)
+char to_uppercase(char ch)
 {
     if (ch >= 'a' && ch <= 'z')
-    {
         return ch - ('a' - 'A');
-    }
+
+    return ch;
+}
+
+char to_lowercase(char ch)
+{
+    if (ch >= 'A' && ch <= 'Z')
+        return ch + ('z' - 'Z');
+
+    return ch;
+}
+
+char swapcase(char ch)
+{
+    if (ch >= 'a' && ch <= 'z')
+        return ch - 32;
+
+    if (ch >= 'A' && ch <= 'Z')
+        return ch + 32;
 
     return ch;
 }
 
 int main()
 {
-    char lower = 'b';
-    char upper = chartoupper(lower);
-    printf("Uppercase: %c\n", upper); // Output: Uppercase: B
-
-    char non_lower = 'Z';
-    char unchanged = chartoupper(non_lower);
-    printf("Unchanged: %c\n", unchanged); // Output: Unchanged: Z
+    printf("Latter casing swaped: %c\n", swapcase('a')); // Case one
+    printf("Latter casing swaped: %c\n", swapcase('B')); // Case two
+    printf("Latter casing swaped: %c\n", swapcase('1')); // Case three
 
     return 0;
 }
